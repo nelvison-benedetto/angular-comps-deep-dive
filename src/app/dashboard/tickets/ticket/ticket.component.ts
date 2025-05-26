b8ef9dba34def5565aa78e64c113e980e09d00d3
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2025-05-26 18:14:57 +0200

    applied content projection + attribute selector

---
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+
+@Component({
+  selector: 'app-ticket',
+  imports: [],
+  templateUrl: './ticket.component.html',
+  styleUrl: './ticket.component.css'
+})
+export class TicketComponent {
+
+}
---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'button[appButton], a[appButton]', //a[appButton]  x ancor tag list abilitate
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
